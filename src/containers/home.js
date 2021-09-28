0885d301e7b3fceed05f2543671a9794abcedf47
Alan Ren <10086000+alanvww@users.noreply.github.com>
2021-09-28 08:52:43 -0400

    Set Router

---
+++ src/containers/home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Home() {
+	return (
+		<main className="App">
+			<header>Weather App</header>
+			<section>Weather Data</section>
+		</main>
+	);
+}
+
+export default Home;
